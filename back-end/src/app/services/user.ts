75ffa3cf1a7c5383e55790a215b59ddab5f281af
Fernando Sena <fernando.sena@mkdata.com.br>
2024-11-25 12:14:27 -0300

    fix: adds error message
    adds message user not found to service

--- back-end/src/app/services/user.ts
+++ back-end/src/app/services/user.ts
@@ -1,15 +1,12 @@
+import MESSAGES from '../helpers/messages';
 import user, { IUser } from '../infrastructure/database/models/user';
 
 const userService = {
-  async getUserById({
-    id,
-  }: {
-    id: string;
-  }): Promise<Partial<IUser> | undefined> {
+  async getUserById({ id }: { id: string }): Promise<Partial<IUser> | string> {
     try {
       const registredUser = await user.findOne({ id });
 
-      if (!registredUser || !registredUser.id) return;
+      if (!registredUser || !registredUser.id) return MESSAGES.USER_NOT_FOUND;
 
       return { id: registredUser.id, name: registredUser.name };
     } catch (error: any) {
